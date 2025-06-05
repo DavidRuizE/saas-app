68e73c20129848aef624cfd6bc6bf1c432c68635
David Ruiz E <druize@eafit.edu.co>
2025-06-05 00:18:27 -0500

    implement companioncard

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CTA from "@/components/CTA";
+import CompanionList from "@/components/CompanionList";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline"> Welcome to my SaaS APP!!</h1>
-      <Button>
-        Let&#39;s get started!
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+        <section className="home-section">
+          <CompanionCard 
+            id="123"
+            name="Neura the Brainy Explorer"
+            topic="Neurol Network of the Brain"
+            subject="science"
+            duration={45}
+            color="#ffda6e"
+            
+          />
+          <CompanionCard 
+            id="456"
+            name="Countsy the Number Wizard"
+            topic="Derivatives and Integrals"
+            subject="math"
+            duration={30}
+            color="#e5d0ff"
+          />
+
+          <CompanionCard 
+            id="789"
+            name="Verba the Vocabulary Builder"
+            topic="language"
+            subject="english literature"
+            duration={45}
+            color="#BDE7FF"
+          />        
+        </section>
+
+        <section className="home-section">
+          <CompanionList />
+          <CTA />
+
+        </section>
+    </main>
   );
 };
 
